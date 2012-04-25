01abba7fc4b37876193daa9aba04048a94d7136a
inbar <inbar@agilesparks.com>
2012-04-25 09:09:32 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c44c88c31054acd17537bbbf6cf95f03b10e15e81962af238a3dfd350ccd19dcbe913c06659f69c5b51e4f248976fb44d9784b243dca8fd0777eca06c64ee3e6'
